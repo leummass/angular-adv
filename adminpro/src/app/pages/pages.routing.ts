e421552b8dcba80732afb50da825ed89706efa88
Samuel <jesuval420@gmail.com>
2024-04-03 03:18:52 -0700

    Avance sección 16

--- adminpro/src/app/pages/pages.routing.ts
+++ adminpro/src/app/pages/pages.routing.ts
@@ -9,6 +9,7 @@ import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
 import { authGuard } from '../guards/auth.guard';
 import { PerfilComponent } from './perfil/perfil.component';
+import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
 
 const routes: Routes = [
   {
@@ -46,6 +47,13 @@ const routes: Routes = [
         path: 'perfil',
         component: PerfilComponent,
         data: { titulo: 'Perfil' },
+      },
+
+      //Mantenimientos
+      {
+        path: 'usuarios',
+        component: UsuariosComponent,
+        data: { titulo: 'Usuarios de la aplicación' },
       },
     ],
   },
--- adminpro/src/app/services/sidebar.service.ts
+++ adminpro/src/app/services/sidebar.service.ts
@@ -12,12 +12,21 @@ export class SidebarService {
       titulo: 'Dashboard',
       icono: 'mdi mdi-gauge',
       submenu: [
-        { titulo: 'Main', url:'/dashboard/'},
-        { titulo: 'ProgressBar', url:'/dashboard/progress'},
-        { titulo: 'Gráficas', url:'/dashboard/grafica1'},
+        { titulo: 'Main', url:''},
+        { titulo: 'ProgressBar', url:'progress'},
+        { titulo: 'Gráficas', url:'grafica1'},
         { titulo: 'Promesas', url:'promesas'},
         { titulo: 'RxJS', url:'rxjs'},
         { titulo: 'Perfil', url:'perfil'}
+      ]
+    },
+    {
+      titulo: 'Mantenimientos',
+      icono: 'mdi mdi-folder-lock-open',
+      submenu: [
+        { titulo: 'Usuarios', url:'usuarios'},
+        { titulo: 'Hospitales', url:'hospitales'},
+        { titulo: 'Médicos', url:'medicos'},
       ]
     }
   ]
